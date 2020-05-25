d7404719db6cbc772e02f0965f41ddb9f79549ce
AbstractUltra <45088170+abstractultra@users.noreply.github.com>
2020-05-25 15:47:38 -0400

    changes

--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -3,11 +3,49 @@ import Greetings from '../../containers/Greetings/Greetings';
 import './Popup.css';
 
 const Popup = () => {
-  const [currentUrl, setCurrentUrl] = useState(' ');
+  const [currentUrl, setCurrentUrl] = useState('');
+  const [phishing, setPhishing] = useState('Checking...');
+  const [probability, setProbability] = useState(0);
+
+  async function scrapeUrl(url) {
+    const response = await fetch(
+      'https://phishy-backend.herokuapp.com/scrape-url',
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          url: url,
+        }),
+      }
+    );
+    const data = await response.json();
+    return data.data;
+  }
+
+  async function getProbability(url) {
+    const body = await scrapeUrl(url);
+    const response = await fetch(
+      `https://phishingnet.benjaminsmith.dev/MLLookup?text=${encodeURIComponent(
+        body
+      )}`,
+      {
+        method: 'POST',
+      }
+    );
+    const data = await response.json();
+    setPhishing(data.Prediction);
+    setProbability(Math.round(data.Probability * 100));
+    return data;
+  }
+
   useEffect(() => {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
       let url = tabs[0].url;
       setCurrentUrl(url);
+      getProbability(url);
     });
   }, []);
 
@@ -21,11 +59,14 @@ const Popup = () => {
         </tr>
         <tr>
           <td id="phishingTitle">Phishing: </td>
-          <td>{}</td>
-        </tr>
-        <tr>
-          <td>Prediction: </td>
-          <td>{}</td>
+          <td>
+            {typeof phishing === 'string'
+              ? phishing
+              : phishing
+              ? 'Phishing'
+              : 'Not Phishing'}
+            , {probability.toString()}% confident
+          </td>
         </tr>
         <tr>
           <td>Certificate:</td>
